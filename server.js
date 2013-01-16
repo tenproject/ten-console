97d185fa011a3e3430d3ef18ba76fc59f5697abd
Adrian Lee <jun.irok@gmail.com>
2013-01-16 09:37:12 -0500

    Reorder middleware execution hierarchy + comments

--- server.js
+++ server.js
@@ -18,16 +18,27 @@ app.set('port', 3000);
 app.set('view engine', 'jade');
 app.locals.pretty = true;
 
-// Express Static Content
+
+// Express Middleware Stack
+// log
+app.use(express.logger('dev'));
+
+// compress response data with gzip/deflate
+app.use(express.compress());
+
+// serve static files
 app.use(express.static(__dirname + '/public', { maxAge: 3600000 }));
 app.use(express.favicon());
 
-// Express Middleware Stack
+// error handler
 app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-app.use(express.logger('dev'));
+
+process.on('uncaughtException', function(err) {
+  console.error(err.stack);
+});
+
+// session support
 app.use(express.cookieParser());
-app.use(express.bodyParser());
-app.use(express.methodOverride());
 app.use(express.session({
   secret: 'ten console secret password',
   // cookie: { httpOnly: true, secure: false },
@@ -38,13 +49,23 @@ app.use(express.session({
     pass: '1561497803dcb7bc34e3e3931267b091'
   })
 }));
+
+// passport for auth
 app.use(passport.initialize());
 app.use(passport.session());
+
+// csrf and middleware for passing token to template
 app.use(express.csrf());
 app.use(function (req, res, next) {
   res.locals.csrftoken = req.session._csrf;
   next();
 });
+
+// parse request body
+app.use(express.bodyParser());
+
+// support _method (e.g. PUT in forms)
+app.use(express.methodOverride());
 
 // expose the "messages" local variable when views are rendered
 // app.use(function(req, res, next){
@@ -69,14 +90,18 @@ app.use(function (req, res, next) {
 //   next();
 // });
 
+// expose template view handlers
 app.use(viewHandler());
+
+// ten app modules
 app.use(login);
 app.use(api);
 app.use(users);
 app.use(slides);
 app.use(locations);
+
+// route middleware
 app.use(app.router);
-app.use(express.compress());
 
 app.get('/', function(req, res) {
   res.render('index', { user: req.user });
@@ -86,10 +111,7 @@ app.get('/console', mixin.ensureAuthenticated, function(req, res) {
   res.render('console', { user: req.user });
 });
 
+// start listen on port 3000
 app.listen(app.get('port'), function () {
   console.log("Express server running on port " + app.get('port'));
-});
-
-process.on('uncaughtException', function(err) {
-  console.error(err.stack);
 });
